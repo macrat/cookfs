0eee1be8c5efe58b3a70145dea3f16cdcbb41524
MacRat <m@crat.jp>
2018-09-16 14:35:35 +0900

    Made polling mechanism

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha512"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"math"
+	"math/rand"
 	"net/http"
+	"net/url"
 	"os"
 	"path"
 	"strconv"
@@ -19,7 +23,10 @@ import (
 const (
 	CHUNK_SIZE         = 64
 	LEADER_DEATH_TIMER = 1 * time.Second
+	LEADER_CHECK_TIMER = 100 * time.Millisecond
 	POLL_TIMEOUT       = 100 * time.Millisecond
+	POLL_WAIT_MINIMAL  = 100 * time.Millisecond
+	POLL_WAIT_MAXIMUM  = 500 * time.Millisecond
 )
 
 func checkHash(hash string, data []byte) bool {
@@ -202,10 +209,29 @@ func (c *ChunkHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type Node struct {
+	URL *url.URL `json:"url"`
+}
+
+func (n Node) endpoint(endpoint string) *url.URL {
+	u := *n.URL
+	u.Path = path.Join(u.Path, "./"+endpoint)
+
+	return &u
+}
+
 type Leader struct {
-	URL         string    `json:"url"`
+	Node
+
 	Term        int64     `json:"term"`
 	LastContact time.Time `json:"last_contact"`
+}
+
+func (l Leader) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		URL  string `json:"url"`
+		Term int64  `json:"term"`
+	}{l.URL.String(), l.Term})
 }
 
 func (l Leader) IsAlive() bool {
@@ -213,15 +239,109 @@ func (l Leader) IsAlive() bool {
 }
 
 type PollInfo struct {
+	URL       *url.URL
 	Term      int64
 	Timestamp time.Time
+}
+
+type AliveMessage struct {
+	URL  *url.URL `json:"url"`
+	Term int64    `json:"term"`
+}
+
+type PollRequest struct {
+	URL  *url.URL `json:"url"`
+	Term int64    `json:"term"`
 }
 
 type LeaderHandler struct {
 	sync.Mutex
 
-	leader Leader
-	poll   PollInfo
+	Self  Node
+	Nodes func() []Node
+
+	leader       Leader
+	aliveMessage []byte
+	poll         PollInfo
+}
+
+func (l *LeaderHandler) sendAliveMessage() {
+	for _, node := range l.Nodes() {
+		go func() {
+			http.Post(node.endpoint("/leader").String(), "application/json", bytes.NewReader(l.aliveMessage))
+		}()
+	}
+}
+
+func (l *LeaderHandler) sendPollRequest() {
+	nodes := l.Nodes()
+
+	request, _ := json.Marshal(PollRequest{
+		URL:  l.Self.URL,
+		Term: l.leader.Term + 1,
+	})
+
+	ballots := make(chan bool, len(nodes))
+
+	for _, n := range nodes {
+		go func(node Node) {
+			resp, err := http.Post(node.endpoint("/leader/poll_request").String(), "application/json", bytes.NewReader(request))
+			ballots <- err == nil && resp.StatusCode == http.StatusNoContent
+		}(n)
+	}
+
+	score := 0
+	for range nodes {
+		if <-ballots {
+			score++
+		}
+	}
+
+	if score >= int(math.Ceil(float64(len(nodes))/2.0)) {
+		l.Lock()
+		defer l.Unlock()
+
+		l.leader = Leader{
+			Node:        l.Self,
+			Term:        l.leader.Term + 1,
+			LastContact: time.Now(),
+		}
+
+		l.aliveMessage, _ = json.Marshal(AliveMessage{
+			URL:  l.leader.URL,
+			Term: l.leader.Term,
+		})
+
+		fmt.Printf("timestamp:%s message:\"I'm leader of %d term\"\n", time.Now(), l.leader.Term)
+	} else {
+		l.aliveMessage = nil
+	}
+}
+
+func (l *LeaderHandler) checkAliveMessage() {
+	if !l.leader.IsAlive() {
+		delay := rand.Int63n((POLL_WAIT_MAXIMUM - POLL_WAIT_MINIMAL).Nanoseconds()) + POLL_WAIT_MINIMAL.Nanoseconds()
+		time.Sleep(time.Duration(delay))
+		l.sendPollRequest()
+	}
+}
+
+func (l *LeaderHandler) watchDog() {
+	tick := time.Tick(LEADER_CHECK_TIMER)
+
+	rand.Seed(time.Now().UnixNano())
+
+	for range tick {
+		if l.aliveMessage != nil {
+			l.sendAliveMessage()
+		} else {
+			l.checkAliveMessage()
+		}
+	}
+}
+
+func (l *LeaderHandler) StartWatchDog() {
+	go l.watchDog()
 }
 
 func (l *LeaderHandler) HandleInfo(w http.ResponseWriter, r *http.Request) {
@@ -241,10 +361,7 @@ func (l *LeaderHandler) HandleInfo(w http.ResponseWriter, r *http.Request) {
 func (l *LeaderHandler) HandleAlive(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Length", "0")
 
-	var req struct {
-		URL  string `json:"url"`
-		Term int64  `json:"term"`
-	}
+	var req AliveMessage
 
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -256,11 +373,17 @@ func (l *LeaderHandler) HandleAlive(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if l.leader.Term != req.Term {
+		fmt.Printf("timestamp:%s message:\"%s be the leader of %d term\"\n", time.Now(), req.URL, req.Term)
+	}
+
 	l.Lock()
 	defer l.Unlock()
 
 	l.leader = Leader{
-		URL:         req.URL,
+		Node: Node{
+			URL: req.URL,
+		},
 		Term:        req.Term,
 		LastContact: time.Now(),
 	}
@@ -271,9 +394,7 @@ func (l *LeaderHandler) HandleAlive(w http.ResponseWriter, r *http.Request) {
 func (l *LeaderHandler) HandlePollRequest(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Length", "0")
 
-	var req struct {
-		Term int64 `json:"term"`
-	}
+	var req PollRequest
 
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -284,6 +405,7 @@ func (l *LeaderHandler) HandlePollRequest(w http.ResponseWriter, r *http.Request
 	defer l.Unlock()
 
 	if l.leader.Term < req.Term && (l.poll.Term < req.Term || l.poll.Timestamp.Add(POLL_TIMEOUT).Before(time.Now())) {
+		l.poll.URL = req.URL
 		l.poll.Term = req.Term
 		l.poll.Timestamp = time.Now()
 
@@ -329,11 +451,15 @@ func NewMux(chunk *ChunkHandler, leader *LeaderHandler) *Mux {
 	m.Handle("/leader", leader)
 	m.Handle("/leader/poll_request", leader)
 
+	leader.StartWatchDog()
+
 	return &Mux{m}
 }
 
 func (m *Mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("timestamp:%s method:%s path:\"%s\"\n", time.Now(), r.Method, r.URL.Path)
+	if !(r.Method == "POST" && r.URL.Path == "/leader") {
+		fmt.Printf("timestamp:%s method:%s path:\"%s\"\n", time.Now(), r.Method, r.URL.Path)
+	}
 
 	if strings.HasSuffix(r.URL.Path, "/") {
 		http.Redirect(w, r, r.URL.Path[:len(r.URL.Path)-1], http.StatusMovedPermanently)
@@ -345,10 +471,6 @@ func (m *Mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	/* TODO
-	"/health" GET/POST
-	"/leader" GET redirect
-	"/leader/alive" POST
-	"/leader/request" POST
 	"/recipe" GET/POST
 	"/recipe/<path/to/recipe>" GET
 	"/recipe/journal" GET/POST
@@ -357,5 +479,23 @@ func main() {
 	"/metrics" GET
 	*/
 
-	http.ListenAndServe(":8080", NewMux(&ChunkHandler{"chunks/"}, &LeaderHandler{}))
+	self_url, err := url.Parse(os.Args[1])
+	if err != nil {
+		panic(err.Error())
+	}
+	self := Node{URL: self_url}
+
+	nodes := []Node{self}
+	for _, u := range os.Args[2:] {
+		parsed, err := url.Parse(u)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		nodes = append(nodes, Node{parsed})
+	}
+
+	http.ListenAndServe(self_url.Host, NewMux(&ChunkHandler{"chunks/"}, &LeaderHandler{Self: self, Nodes: func() []Node {
+		return nodes
+	}}))
 }
