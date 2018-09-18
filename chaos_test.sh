e757d0af85074acede0377f1baee0fda35409528
MacRat <m@crat.jp>
2018-09-18 21:58:57 +0900

    Moved subdirectory(experimental code) to root(main code)

--- chaos_test.sh
+++ chaos_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IDS=`echo {1..5}`
+IDS=`echo {1..50}`
 BASE_PORT=8000
 
 function generate() {
@@ -15,17 +15,25 @@ function generate() {
 	  echo "    image: golang:alpine"
 	  echo "    volumes: ['./:/cookfs']"
 	  echo "    ports: ['$((${BASE_PORT} + ${ID} - 1)):80']"
-	  echo "    command: ash -c 'go run /cookfs/*.go http://cookfs_${ID}:80 ${HOSTS}'"
+	  echo "    command: ash -c 'cd /cookfs && go run `echo $(ls *.go | grep -v _test.go)` http://cookfs_${ID}:80 ${HOSTS}'"
 	  echo
 	done
 }
 
-function leader() {
+function info() {
 	for ID in ${IDS}; do
 		printf "${ID}: "
-		curl --silent http://localhost:$((${BASE_PORT} + ${ID} - 1))/leader
+		curl --silent --max-time 0.1 http://localhost:$((${BASE_PORT} + ${ID} - 1))/
 		echo
 	done
+}
+
+function stop_random() {
+	docker-compose stop $(echo `echo ${IDS} | grep -o '[0-9]\+' | sort -R | head -n ${1:-1} | xargs -I{} echo cookfs_{}`)
+}
+
+function stop_leader() {
+	docker-compose stop cookfs_`curl -s http://localhost:${BASE_PORT}/ | jq -r .leader | sed -e 's/http:\/\/cookfs_//' -e 's/:80//'`
 }
 
 function show_help() {
@@ -33,15 +41,19 @@ function show_help() {
 	echo
 	echo 'COMMAND:'
 	echo '  start - run containers'
-	echo '  stop - stop containers'
+	echo '  stop - stop and remove containers'
+	echo '  stop_random [number] - stop random containers'
+	echo '  stop_leader - stop leader container'
 	echo '  gen - generate docker-compose.yml'
-	echo '  leader - get leader information'
+	echo '  info - get current term information'
 }
 
 case $1 in
 	start ) generate > docker-compose.yml && docker-compose up -d ;;
 	stop ) generate > docker-compose.yml && docker-compose down ;;
+	stop_random ) stop_random $2 ;;
+	stop_leader ) stop_leader ;;
 	gen ) generate ;;
-	leader ) leader ;;
+	info ) info ;;
 	* ) show_help $0 >&2 ;;
 esac
